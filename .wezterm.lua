95c7150264c2510e9a07a2f37c97d12b80284cbc
Chris Kindler <christian_kindler@web.de>
2026-03-06 19:24:45 +0000

    Updated wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,24 +3,8 @@ local act = wezterm.action
 
 local config = {
 	font = wezterm.font("Cousine"),
-	-- font = wezterm.font("Menlo"),
-	-- font = wezterm.font("CommitMono"),
-	-- font = wezterm.font("Liberation Mono"),
-	-- font = wezterm.font("Geist Mono"),
-	-- font = wezterm.font("Fragment Mono"),
-	-- font = wezterm.font("Roboto Mono"),
-    
-    color_scheme = 'Tokyo Night',
-	-- color_scheme = 'Derp (terminal.sexy)',
-	-- color_scheme = 'Nucolors (terminal.sexy)',
-    -- color_scheme = 'Kasugano (terminal.sexy)',
-    -- color_scheme = 'Dracula',
-    --color_scheme = 'Cloud (terminal.sexy)',
-	-- color_scheme = '3024 (dark) (terminal.sexy)',
-	--color_scheme = 'Default (dark) (terminal.sexy)',
 	font_size = 17,
 	line_height = 0.90,
-	-- line_height = 0.75,
 	cell_width = 1.0, 
 	window_decorations = "RESIZE",
 	enable_tab_bar = false,
@@ -28,14 +12,8 @@ local config = {
     warn_about_missing_glyphs = false,
 	
 	colors = {
-        -- slightly softer default text
-        foreground = "#c8c8c8",
-        background = "#000000",
-
-        -- make cursor readable
-        cursor_fg = "#000000",  -- text inside cursor
-        -- optional but recommended:
-        cursor_bg = "#ffffff",  -- cursor block color
+        cursor_fg = "#000000",
+        cursor_bg = "#ffffff",
         cursor_border = "#ffffff",
     }	
 }
